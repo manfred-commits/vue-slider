c4890804bd73160999d8c6a522ad3f08e506eda6
manfred-commits <manfredipiraino@gmail.com>
2021-09-17 16:41:55 +0200

    adde basic image object connected to img tag

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const app = new Vue({
 
     el:"#root",
     data:{
+        images:["https://upload.wikimedia.org/wikipedia/commons/5/58/Sunset_2007-1.jpg", "https://i.redd.it/nwxarpze09o21.jpg","https://images.wallpaperscraft.com/image/single/new_york_night_city_skyline_121546_3840x2160.jpg"],
+        imagesIndex:0,
 
     },
     methods:{
