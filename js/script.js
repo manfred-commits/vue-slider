f7ad85272839c6047fbea8d29bd0bf4ae9601e64
manfred-commits <manfredipiraino@gmail.com>
2021-09-17 16:30:35 +0200

    added basic html & css + vueJS start

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@
 // Rifare l'esercizio dello slider come fatto assieme in classe.
 // Bonus:
 // Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
+const app = new Vue({
+
+    el:"#root",
+    data:{
+
+    },
+    methods:{
+
+    },
+    mounted:{
+
+    }
+
+
+});
